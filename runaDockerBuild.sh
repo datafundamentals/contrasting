d775b431fc20e164f13ee0516c3d473051ef2d09
petecarapetyan <pete@couldbe.net>
2016-01-27 11:39:57 -0600

    more tweaks to runaDocker:

--- runaDockerBuild.sh
+++ runaDockerBuild.sh
@@ -1,3 +1,6 @@
+cd ~/work/contrasting
 docker build -t contrast/pre_a .
 docker images
 docker ps
+docker run -it -d -p 8121:8942 --name contrasting_a contrast/pre_a
+echo "at this point you should be able to see JSON at http://192.168.33.13:8121 from a local browser"
